9c5601564eb37d07dcd0a764e16cd0a0af38ffb4
SonnySon <58923633+SonnySon-P@users.noreply.github.com>
2025-04-14 23:00:37 +0800

    Add files via upload

---
+++ Lesson 10/erosion.py
@@ -0,0 +1,19 @@
+import cv2
+
+# 輸入影像檔案的路徑
+imagePath = "image.jpg"
+
+# 讀取影像
+image = cv2.imread(imagePath)
+
+# BGR轉換為灰階與創建結構元素
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (11, 11))
+
+# 膨脹處理
+image = cv2.dilate(image, kernel)
+cv2.imshow("侵蝕處理影像", image)
+
+# 等待按鍵事件，按任意鍵關閉影像視窗
+cv2.waitKey(0)  # 按下任意鍵
+cv2.destroyAllWindows()  # 結束所有影像視窗
